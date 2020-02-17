cb8cfddc0f4bc515edc5347af7dd2a07dbb86da8
marcelljoe <marselljonathan@gmail.com>
2020-02-17 16:53:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zs6g9d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zs6g9d)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { CartService } from '../cart.service';
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items;
+  checkoutForm;
+  constructor(
+    private cartService : CartService,
+    private formBuilder : FormBuilder
+  ) { 
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    })
+  }
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData){
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+    window.alert("Thanks for purchasing our products.");
+    console.warn("Your order has been submitted", customerData);
+  }
+
+}
